8dd102147c4a21a2a16afed889a2ab5542def3da
Suman Das <sum4ndas@gmail.com>
2025-10-01 18:41:20 +0530

    Add location controller to check character's location

---
+++ controller/locationController.js
@@ -0,0 +1,37 @@
+const { PrismaClient } = require("../generated/prisma");
+
+const prisma = new PrismaClient();
+
+exports.getCharacter = async (req, res) => {
+  // get click position form query parameters
+  const { x: clickX, y: clickY } = req.query;
+  console.log({ clickX, clickY });
+
+  // get the character name from path parameter
+  const characterName = req.params.characterName;
+  // get character details
+  const character = await prisma.character.findUnique({
+    where: { name: characterName },
+  });
+
+  // check if the click location is within the character box OR
+  // click is on the character location box
+  if (character && clickX >= character.xLeft && clickX <= character.xRight) {
+    if (clickY >= character.yTop && clickY <= character.yBottom) {
+      res
+        .status(201)
+        .json({ success: true, message: `${character.name} found`, character });
+    }
+  }
+
+  // If the click is not on the character box
+  if (character) {
+    res.json({
+      success: false,
+      message: `${character.name} not found`,
+      character,
+    });
+  } else {
+    res.status(404).json({ success: false, message: "Wrong search" });
+  }
+};
