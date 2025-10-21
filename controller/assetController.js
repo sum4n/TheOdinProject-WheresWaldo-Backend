88655abc1e560727656abfd404124b638a1db2ec
Suman Das <sum4ndas@gmail.com>
2025-10-21 20:53:13 +0530

    feat(controller): include characters and initialize session in getGameAssets

--- controller/assetController.js
+++ controller/assetController.js
@@ -3,6 +3,17 @@ const prisma = new PrismaClient();
 
 exports.getGameAssets = async (req, res) => {
   const gameBoard = await prisma.gameboard.findFirst();
+  const characters = await prisma.character.findMany({
+    select: { id: true, name: true, imgUrl: true },
+  });
 
-  res.status(200).json(gameBoard);
+  // set custom properties
+  req.session.characterCount = characters.length;
+  req.session.startTime = Date.now();
+
+  // console.log(req.session);
+  // console.log(characters);
+  // console.log(gameBoard);
+
+  res.status(200).json({ gameBoard, characters });
 };
