c2307b04b82855444a85c078684826034041afc5
Suman Das <sum4ndas@gmail.com>
2025-10-29 20:25:19 +0530

    feat(middleware): parse incoming json POST request bodies

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
+
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET"],
+    methods: ["GET", "POST"],
   })
 );
 
