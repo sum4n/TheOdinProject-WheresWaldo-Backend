e6ce1caa1fc5757024420480077767de24c511ae
Suman Das <sum4ndas@gmail.com>
2025-12-12 19:16:19 +0530

    refactor(routes): rename asset.js route to gameboard.js

--- routes/asset.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const router = Router();
-
-const assetController = require("../controller/assetController");
-
-router.get("/assets", assetController.getGameAssets);
-
-module.exports = router;
