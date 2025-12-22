c1c6046fe815b25432e6525a7380b54fcf6e3e05
Suman Das <sum4ndas@gmail.com>
2025-12-22 18:28:20 +0530

    feat(controller): send characters with the boards

--- controller/gameboardController.js
+++ controller/gameboardController.js
@@ -2,6 +2,16 @@ const { PrismaClient } = require("../generated/prisma");
 const prisma = new PrismaClient();
 
 exports.getGameBoards = async (req, res) => {
-  const gameBoards = await prisma.gameboard.findMany();
+  const gameBoards = await prisma.gameboard.findMany({
+    include: {
+      characters: {
+        select: {
+          id: true,
+          name: true,
+          imgUrl: true,
+        },
+      },
+    },
+  });
   res.status(200).json(gameBoards);
 };
