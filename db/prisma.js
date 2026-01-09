87cf62ab6aec427ee76171fea85d74daaad5767b
Suman Das <sum4ndas@gmail.com>
2026-01-09 18:43:54 +0530

    feat(db): creat prisma client file that selects db URL based on environment

---
+++ db/prisma.js
@@ -0,0 +1,16 @@
+const { PrismaClient } = require("@prisma/client");
+
+const databaseUrl =
+  process.env.NODE_ENV === "test"
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL;
+
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: databaseUrl,
+    },
+  },
+});
+
+module.exports = prisma;
