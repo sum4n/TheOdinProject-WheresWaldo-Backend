0805d340a47a48650ebb0877ca39111fad23c649
Suman Das <sum4ndas@gmail.com>
2026-02-04 15:12:51 +0530

    refactor: remove server.js and start the app from app.js

--- app.js
+++ app.js
@@ -42,4 +42,12 @@ app.use((err, req, res, next) => {
   });
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log(`App is running on port: ${PORT}`);
+});
