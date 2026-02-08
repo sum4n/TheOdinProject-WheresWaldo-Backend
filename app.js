7695c89d3f3fac3f42d1c24d665c8def4e220a53
Suman Das <sum4ndas@gmail.com>
2026-02-08 19:14:47 +0530

    feat(db): use prisma session store

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const session = require("express-session");
 const cors = require("cors");
+const prisma = require("./db/prisma");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
 
 const app = express();
 
@@ -19,6 +21,14 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
   }),
 );
 
